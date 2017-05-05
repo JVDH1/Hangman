545b8b2a3bc4b632222fcd16308509d0b590363f
Johan van der Hoeven <johan.hoeven@outlook.com>
2017-05-05 16:37:56 +0200

    Jar file now opens Command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args){
-        Play.play();
+        boolean x = false;
 
+        for(String s : args){
+            if(s.equals("a")){
+                x = true;
+                break;
+            }
+        }
+
+        if(x){
+            Play.play();
+        } else {
+            try {
+                String file = Thread.currentThread().getContextClassLoader().getResource("Main.class").toString();
+                String dir = file.substring(6, file.length()-11);
+                Runtime.getRuntime().exec("cmd /k start cmd /k \"cd C:/Users/Johan/IdeaProjects/Hangman/target/classes && java Main a\"");
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/RandomWord.java
+++ src/main/java/RandomWord.java
@@ -9,16 +9,13 @@ public class RandomWord {
     private static void loadFile(){
         // If allWords is empty fill it with words
         if(allWords[0] == null) {
-            File file = new File("C:\\Users\\Johan\\IdeaProjects\\hangman\\src\\main\\resources\\words.txt");
-            FileInputStream fis;
+            InputStream is;
             BufferedInputStream bis;
             DataInputStream dis;
 
             try {
-                fis = new FileInputStream(file);
-
-                // Add BufferedInputStream for fast reading
-                bis = new BufferedInputStream(fis);
+                is = Thread.currentThread().getContextClassLoader().getResourceAsStream("\\words.txt");
+                bis = new BufferedInputStream(is);
                 dis = new DataInputStream(bis);
 
                 int x = 0;
@@ -31,7 +28,7 @@ public class RandomWord {
                 }
 
                 // Dispose of all the resources after using them
-                fis.close();
+                is.close();
                 bis.close();
                 dis.close();
 
